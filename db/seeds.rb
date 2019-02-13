29b96f71abc0c3805b0310820b2978a60e984b48
guillaume imary <guillaume.imary@gmail.com>
2019-02-13 15:43:45 +0100

    yelp with errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Restaurant.create(name: 'mario e luigi pasta', address: '10 rue Oberkampf', category: 'italian', phone_number: '06 07 07 07 07')
+Restaurant.create(name: 'ping pong', address: '11 rue Oberkampf', category: 'chinese', phone_number: '06 07 07 07 08')
+Restaurant.create(name: 'baguette fromage', address: '12 rue Oberkampf', category: 'french', phone_number: '06 07 07 07 09')
+Restaurant.create(name: 'pas dsushi', address: '13 rue Oberkampf', category: 'japanese', phone_number: '06 07 07 07 10')
+Restaurant.create(name: 'moule frite', address: '14 rue Oberkampf', category: 'belgian', phone_number: '06 07 07 07 11')
